202128a1fee116979a1d143526fc93572af099ef
ntx03 <ntx033@yandex.ru>
2023-08-08 12:31:27 +0500

    add interceptors in

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,10 @@ import {USER_LOCALSTORAGE_KEY} from "shared/const/localstorage";
 // const baseURL = __IS_DEV__ ? "http://localhost:8000" : "https://praduction.ru";
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        Authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? "",
-    },
 });
+$api.interceptors.request.use((config)=> {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? "";
+    }
+    return config;
+})
